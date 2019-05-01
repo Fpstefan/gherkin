488022cd7b3b029b3717d2e818e508233a1f17e9
Fpstefan <48359562+Fpstefan@users.noreply.github.com>
2019-05-01 23:02:26 +0200

    Delete read_profile.clj

--- read_profile.clj
+++
@@ -1,8 +0,0 @@
-(use 'clojure.pprint)
-
-(defn read-profile [f]
-  (let [stats (read-string (str "[" (slurp f) "]"))
-        called (into #{} (map second stats))
-        call-time (fn call-time [x] (partition 2 (filter #(= (second %) x) ents)))
-        total-time (fn [x] (reduce + (map (fn [[[_ _ start] [_ _ end]]] (- end start)) (call-time x))))]
-    (pprint (into (sorted-map) (map #(vector (total-time %) %) called)))))
